7736e36942701c148344505919264195d4f43e8f
Dmytro Byrchenko <byrchenko@gmail.com>
2024-08-16 14:23:36 +0300

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const logger = require("morgan");
 
 const playerRouter = require("./routes/player");
