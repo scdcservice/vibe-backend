efd754321c64331d96b553e8bea4b05fea56517e
Dmytro Byrchenko <byrchenko@gmail.com>
2024-08-27 19:54:30 +0300

    added loggers

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,9 @@
 export function GET(request) {
+  console.log(request);
   return new Response(`Hello from webhook ${JSON.stringify(request, 4)}`);
 }
 
 export function POST(request) {
+  console.log(request);
   return new Response(`Hello from webhook ${JSON.stringify(request, 4)}`);
 }
