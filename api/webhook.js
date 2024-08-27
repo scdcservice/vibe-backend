df126052a3441136e039fe9cd1d6563aa514c05c
Dmytro Byrchenko <byrchenko@gmail.com>
2024-08-27 19:52:35 +0300

    added post webhook request

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,7 @@
 export function GET(request) {
   return new Response(`Hello from webhook ${JSON.stringify(request, 4)}`);
 }
+
+export function POST(request) {
+  return new Response(`Hello from webhook ${JSON.stringify(request, 4)}`);
+}
